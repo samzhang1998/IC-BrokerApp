93c69af41661549e5ba76d31cae9211b0b7a4da9
hzsnq <44885942+hzsnq@users.noreply.github.com>
2025-06-05 22:58:58 +0800

    feat:添加tabbar，添加4大模块主页面

---
+++ src/pages/application/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useLocale } from '@/hooks/useLocale'
+import { useUser } from '@/hooks/useUser'
+const { token, roles } = useUser()
+const { langStatus } = useLocale()
+
+onMounted(() => {})
+
+onShow(() => {})
+
+const navBar = ref({
+  isNotification: true,
+  backgroundColor: '#fff'
+})
+</script>
+
+<template>
+  <view class="common-bg home">
+    <NavBar v-bind="navBar">
+      <template #content>
+        <view class="bar-title act">Application</view>
+      </template>
+      <template #right> </template>
+    </NavBar>
+
+    <!-- <NoData v-if="productAll.length <= 0"></NoData> -->
+
+    <TabBar></TabBar>
+  </view>
+</template>
+
+<style lang="scss" scoped></style>
--- src/store/modules/tabBar.ts
+++ src/store/modules/tabBar.ts
@@ -1,23 +1,10 @@
-/*
- * @Author: taoyongjian taoyongjian-zf@bjebc.com
- * @Date: 2023-01-20 14:15:04
- * @LastEditors: eric taoyongjian-zf@bjebc.com
- * @LastEditTime: 2023-11-02 08:14:17
- * @FilePath: /bhg-app-client/src/store/modules/tabBar.ts
- * @Description:
- *
- * Copyright (c) 2023 by gome, All Rights Reserved.
- */
 import store from '@/store'
-import club from '@/static/icon/club.png'
-import event from '@/static/icon/event.png'
-import home from '@/static/icon/home.png'
-import investment from '@/static/icon/investment.png'
+import dashboard from '@/static/icon/dashboard.png'
+import application from '@/static/icon/application.png'
+import product from '@/static/icon/product.png'
 import user from '@/static/icon/user.png'
-import clubAct from '@/static/icon/club-act.png'
-import eventAct from '@/static/icon/event-act.png'
-import homeAct from '@/static/icon/home-act.png'
-import investmentAct from '@/static/icon/investment-act.png'
+import dashboardAct from '@/static/icon/dashboard-act.png'
+import applicationAct from '@/static/icon/application-act.png'
 import userAct from '@/static/icon/user-act.png'
 export const useTabBarStore = defineStore({
   id: 'tabBar',
@@ -25,42 +12,35 @@ export const useTabBarStore = defineStore({
     return {
       tabBarList: [
         {
-          key: 'home',
-          name: 'Home',
-          iconPath: home,
-          iconActivePath: homeAct,
-          router: '/pages/index/index'
+          key: 'dashboard',
+          name: 'Dashboard',
+          iconPath: dashboard,
+          iconActivePath: dashboardAct,
+          router: '/pages/dashboard/index'
         },
         {
-          key: 'event',
-          name: 'Event',
-          iconPath: event,
-          iconActivePath: eventAct,
-          router: '/pages/event/index'
+          key: 'application',
+          name: 'Application',
+          iconPath: application,
+          iconActivePath: applicationAct,
+          router: '/pages/application/index'
         },
         {
-          key: 'investment',
-          name: 'Investment',
-          iconPath: investment,
-          iconActivePath: investmentAct,
-          router: '/pages/investment/index'
+          key: 'product',
+          name: 'Product',
+          iconPath: product,
+          iconActivePath: dashboardAct,
+          router: '/pages/product/index'
         },
         {
-          key: 'club',
-          name: 'Club',
-          iconPath: club,
-          iconActivePath: clubAct,
-          router: '/pages/club/index'
-        },
-        {
-          key: 'user',
-          name: 'For you',
+          key: 'profile',
+          name: 'Profile',
           iconPath: user,
           iconActivePath: userAct,
-          router: '/pages/user/index'
+          router: '/pages/profile/index'
         }
       ],
-      selectKey: 'home'
+      selectKey: 'dashboard'
     }
   },
   actions: {
