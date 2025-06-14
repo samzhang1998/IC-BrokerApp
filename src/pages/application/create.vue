57edf2c9e11ec81ad1b35044c9c20cd335c49f90
AndyKong <andy_kong@163.com>
2025-06-14 23:54:53 +0800

    create application Navbar

--- src/pages/application/create.vue
+++ src/pages/application/create.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
+  <view>
+    <NavBar v-bind="navBar">
+      <template #content>
+        <view class="bar-title act">Create Application</view>
+      </template>
+      <template #right> </template>
+    </NavBar>
     <wd-button>主要按钮</wd-button>
     <wd-button type="success">成功按钮</wd-button>
     <wd-button type="info">信息按钮</wd-button>
     <wd-button type="warning">警告按钮</wd-button>
     <wd-button type="error">危险按钮</wd-button>
-  </div>
+  </view>
 </template>
 
 <script setup lang="ts">
-
+const navBar = ref({
+  isNotification: true,
+  backgroundColor: '#fff'
+})
 </script>
 
 <style scoped>
