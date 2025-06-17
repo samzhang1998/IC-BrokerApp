db6adc5d486e9daf43eb099e98eba585a6236c8e
AndyKong <andy_kong@163.com>
2025-06-17 23:22:45 +0800

    application summary

---
+++ src/pages/application/borrowerSummary.vue
@@ -0,0 +1,17 @@
+<template>
+  <BasePage title="Create Application" hasBack>
+    <view class="border border-#E8EBEE border-solid rounded-lg px-3 py-3 bg-#FCFCFC">
+      <view class="flex-y-center gap-3">
+        <check-badge></check-badge>
+        <text class="text-28rpx font-bold">Personal Applicants</text>
+      </view>
+      <view class="flex-col mt-3 gap-1">
+        Add all person applicants that are either borrowers or guarantors to this application
+      </view>
+    </view>
+  </BasePage>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
